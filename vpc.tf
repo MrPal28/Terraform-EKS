3206013c153a001e5fe3e265767c3907043a7607
MrPal28 <arindampal669@gmail.com>
2026-09-21 12:04:08 +0000

    local add and vpc update

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,19 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "ap-south-1-vpc"
-  cidr = "10.0.0.0/16"
+  name = local.name
+  cidr = local.vpc_cidr
 
-  azs             = ["ap-south-1a", "ap-south-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
+
   tags = {
     Terraform = "true"
-    Environment = var.environment
+    Environment = local.env
   }
 }
